4c130b19f14d865185ef3d092efae873195ad369
jonytrifulca <jonytrifulca@hotmail.com>
2019-11-29 11:00:51 +0100

    update url conexion remota

--- server/config/config.js
+++ server/config/config.js
@@ -9,12 +9,12 @@ process.env.NODE_ENV = process.env.NODE_ENV || 'dev';
 
 
 
-//base de datos //
+//base de datos 
 let urlDB;
 
 if (process.env.NODE_ENV == 'dev')
     urlDB = 'mongodb://localhost:27017/cafe';
 else
-    urlDB = 'mongodb://pacouser:pacouser@cluster0-prrvi.mongodb.net/test?retryWrites=true&w=majority';
+    urlDB = 'mongodb+srv://pacouser:pacouser@cluster0-prrvi.mongodb.net/test?retryWrites=true&w=majority';
 
 process.env.URLDB = urlDB;
